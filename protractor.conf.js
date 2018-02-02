13feabf7f974319d95d16261231675c95a4516e2
Eko Eryanto <ekoeryanto@gmail.com>
2018-02-03 04:41:23 +0700

    chore: headless test

--- protractor.conf.js
+++ protractor.conf.js
@@ -9,8 +9,12 @@ exports.config = {
     './e2e/**/*.e2e-spec.ts'
   ],
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'chrome',
+    chromeOptions: {
+      args: [ "--headless", "--disable-gpu", "--window-size=800x600" ]
+    }
   },
+
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
