37eb7a4216f7d64e621adbfa726af29f03023924
Eko Eryanto <ekoeryanto@gmail.com>
2018-02-03 02:18:46 +0700

    initialize project

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'xo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'xo';
+}
